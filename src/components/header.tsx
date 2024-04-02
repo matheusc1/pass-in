0d197e8500ca5322f1e5d3e3706c0fb47e298457
matheusc1 <cardoso.matheusbs@gmail.com>
2024-04-02 19:42:50 -0300

    feat: create the initial structure of the project

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import Logo from '../assets/logo.svg'
+
+export function Header() {
+  return (
+    <div className="flex items-center gap-5 py-2">
+      <img src={Logo} />
+
+      <nav className="flex items-center gap-5">
+        <a className="font-medium text-sm text-zinc-300" href="#">Eventos</a>
+        <a className="font-medium text-sm" href="#">Participantes</a>
+      </nav>
+    </div>
+  )
+}
